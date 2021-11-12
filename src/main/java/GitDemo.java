c16863268ab42371dc78ee443d31a0c8c4cd469e
xiaoyoung01 <94050985+xiaoyoung01@users.noreply.github.com>
2021-11-12 15:20:28 +0800

    pull test

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -9,5 +9,6 @@ public class GitDemo {
         System.out.println("push test");
         System.out.println("push test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
