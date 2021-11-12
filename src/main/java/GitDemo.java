4a9fbc659cd09d9b5f5a750ea6dffcfb48751084
xiaoyoung <xiaoyoung@163.com>
2021-11-12 14:01:32 +0800

    test1

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -2,5 +2,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("git测试");
         System.out.println("git测试");
+        System.out.println("git test");
     }
 }
